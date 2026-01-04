c2c22b07d41c9a2ea8d52890f1b859aea3525c71
wattakeshi <takeshi.watanabe@live.com>
2026-01-04 16:12:29 +0900

    add: ZOD / todoSchema

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,7 +1,6 @@
 import type { Request, Response } from "express";
-
+import { todoSchema } from "../schemas/todoSchema.js";
 import { data } from "../data.js"
-
 let idCounter = 4
 
 export const TodoController = {
@@ -11,7 +10,14 @@ export const TodoController = {
     },
     //post
     create: (req: Request, res: Response) => {
-        const { taskname, done } = req.body;
+        const result = todoSchema.safeParse(req.body)
+        if (!result.success) {
+            return res.status(400).json({
+                message: "erro de validação, dados insuficientes",
+                details: result.error.format()
+            })
+        }
+        const { taskname, done } = result.data;
         const newTask = { taskname, done, id: idCounter }
         data.push(newTask);
         idCounter++
@@ -19,22 +25,21 @@ export const TodoController = {
     },
     //PUT
     update: (req: Request, res: Response) => {
-        const { taskname, done } = req.body;
-        const { id } = req.params;
-        if (taskname === undefined || done === undefined) {
-            return res.status(400).send({ message: "Taskname or done:status undefined." })
+        const result = todoSchema.safeParse(req.body);
+        if (!result.success) {
+            return res.status(400).send({ message: "Erro de validação, dados insuficientes" })
         }
+        const { taskname, done } = result.data;
+        const { id } = req.params;
         const index = data.findIndex(tsk => tsk.id === Number(id))
         if (index === -1) {
             return res.status(404).send({ message: "task not found" })
         }
-
         const newTask = {
             taskname: taskname,
             done: done,
             id: Number(id)
         }
-
         data[index] = newTask
         return res.status(202).send({ message: "Task changed succesfully!" })
     },
---
+++ src/schemas/todoSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const todoSchema = z.object({
+    taskname: z.string()
+        .min(3, { message: "O nome deve ter pelo menos 3 caracteres" })
+        .max(100, { message: "O nome está muito longo" }),
+    done: z.boolean().refine(val => val === undefined, { message: "O status done é obrigatório" })
+
+});
+
+export type TaskInput = z.infer<typeof todoSchema>
